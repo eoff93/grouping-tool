07fe87ccc783842761f4db6257ff64a5f45161ed
eoff93 <bane.dev101@gmail.com>
2016-07-11 14:22:13 +0200

    Add mock sites and groups

--- app/app.js
+++ app/app.js
@@ -1,3 +1,44 @@
 'use strict';
 
-var app = angular.module('myApp',[]);
+angular.module('groupApp',[])
+  .controller('ListController', function() {
+    var vm = this;
+
+    vm.sites = [
+      {
+        id: 1,
+        url: 'google.com',
+        color: 'green',
+        groups: []
+      },
+      {
+        id: 2,
+        url: 'facebook.com',
+        color: 'blue',
+        groups: []
+      },
+      {
+        id: 3,
+        url: 'twitter.com',
+        color: 'red',
+        groups: []
+      },
+    ];
+
+    vm.groups = [
+      {
+        id: 1,
+        name: 'Search engines',
+        color: "",
+        sites: []
+      },
+      {
+        id: 2,
+        name: 'Finance sites',
+        color: "green",
+        sites: []
+      }
+    ];
+  });
+
+;
