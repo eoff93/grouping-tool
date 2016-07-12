4b1b2eed6d558cdc85daed9ccdd6c9b5f3829b8a
eoff93 <bane.dev101@gmail.com>
2016-07-12 11:22:40 +0200

    Implement group adding functionality

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@
 angular.module('groupApp',[])
   .controller('MainController', function() {
     var vm = this;
+
+    // adds a website from a modal and resets the forms
     vm.addSite = function() {
       vm.sites.push({
         id: vm.sites.length + 1,
@@ -14,20 +16,17 @@ angular.module('groupApp',[])
       vm.color = '';
     }
 
-    vm.sites = [];
+    // adds a group from a modal and resets the forms
+    vm.addGroup = function() {
+      vm.groups.push({
+        id: vm.groups.length + 1,
+        name: vm.name,
+        color: vm.color
+      });
+      vm.name = '';
+      vm.color = '';
+    }
 
-    vm.groups = [
-      {
-        id: 1,
-        name: 'Search engines',
-        color: "",
-        sites: []
-      },
-      {
-        id: 2,
-        name: 'Social sites',
-        color: "",
-        sites: []
-      }
-    ];
+    vm.sites = [];
+    vm.groups = [];
   });
