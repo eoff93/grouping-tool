c1e4fbc0a6839c8a9b148af62d40ea3b6f57ec86
eoff93 <bane.dev101@gmail.com>
2016-07-15 19:11:56 +0200

    Pull group logic to a separate controller

--- app/app.js
+++ app/app.js
@@ -24,7 +24,68 @@ angular.module('groupApp',['checklist-model'])
 
 
   })
+  .controller('GroupController', function(getData) {
+    var vm = this;
+    vm.groups = getData.groups;
 
+    vm.setGroupToEdit = function(index) {
+      vm.groupToEdit = vm.groups[index - 1];
+    }
+
+    // cycles through sites and removes the parameter group
+    vm.removeGroupFromSites = function(group) {
+      for (var i = 0; i < vm.sites.length; i++) {
+        for (var j = 0; j < vm.sites[i].groups.length; j++) {
+          if (vm.sites[i].groups[j] === group) {
+            vm.sites[i].groups.splice(j, 1);
+          }
+        }
+      }
+    }
+
+    // cycles through sites and removes the parameter group
+    vm.removeGroupFromSites = function(group) {
+      for (var i = 0; i < vm.sites.length; i++) {
+        for (var j = 0; j < vm.sites[i].groups.length; j++) {
+          if (vm.sites[i].groups[j] === group) {
+            vm.sites[i].groups.splice(j, 1);
+          }
+        }
+      }
+    }
+
+    // updates group ids starting from 1
+    vm.updateGroupIds = function() {
+      for (var i = 0; i < vm.groups.length; i++) {
+        vm.groups[i].id = i + 1;
+      }
+    }
+
+    vm.removeGroup = function(activeGroup, group) {
+      vm.removeGroupFromSites(group);
+      vm.groups.splice(activeGroup, 1);
+      vm.updateGroupIds();
+      vm.selectTab(vm.tab - 1);
+    }
+
+    // only removes a website from its group, not from all websites
+    vm.removeFromGroupOnly = function(site) {
+      var index = vm.groups[vm.tab - 1].sites.indexOf(site);
+      vm.groups[vm.tab - 1].sites.splice(index, 1);
+    }
+
+    // adds a group from a modal and resets the forms
+    vm.addGroup = function() {
+      vm.groups.push({
+        id: vm.groups.length + 1,
+        name: vm.name,
+        color: vm.color,
+        sites: []
+      });
+      vm.name = '';
+      vm.color = '';
+    }
+  })
   .controller('AppController', function(getData) {
     var vm = this;
     vm.data = getData;
@@ -34,16 +95,6 @@ angular.module('groupApp',['checklist-model'])
     vm.siteToEdit;
     vm.groupToEdit;
     vm.tab = 0;
-
-    // changes vm.tab to the id of a clicked group
-    vm.selectTab = function(setTab) {
-      vm.tab = setTab;
-    };
-
-    // checks which tab is selected and then use it to set a class
-    vm.isSelected = function(checkTab) {
-      return vm.tab === checkTab;
-    };
 
     // adds a website from a modal and resets the forms
     vm.addSite = function() {
